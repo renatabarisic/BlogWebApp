909912cc3ed6d10954e54df6ab5b1698fc8e7f8b
renatabarisic <rbarisic@etfos.hr>
2023-09-03 18:16:45 +0200

    Create API

---
+++ Blog.API/Blog.API/Blog.API/Controllers/CategoriesController.cs
@@ -0,0 +1,42 @@
+﻿using Blog.API.Data;
+using Blog.API.Models.Domain;
+using Blog.API.Models.DTO;
+using Blog.API.Repositories.Interface;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Blog.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoriesController : ControllerBase
+    {
+        private readonly ICategoryRepository categoryRepository;
+
+        public CategoriesController(ICategoryRepository categoryRepository)
+        {
+            this.categoryRepository = categoryRepository;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateCategory(CreateCategoryRequestDto request)
+        {
+            var category = new Category
+            {
+                Name = request.Name,
+                UrlHandle = request.UrlHandle
+            };
+
+            await categoryRepository.CreateAsync(category);
+
+            var resonse = new CategoryDto
+            {
+                Id = category.Id,
+                Name = category.Name,
+                UrlHandle = category.UrlHandle
+            };
+
+            return Ok(resonse);
+        }
+    }
+}
---
+++ Blog.API/Blog.API/Blog.API/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Blog.API.Models.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace Blog.API.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+        }
+        public DbSet<Post> Posts { get; set; }
+        public DbSet<Category> Categories { get; set; }
+    }
+}
